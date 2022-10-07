b9de9e30619236128b4a3a34ad2294d6749b0778
Megan Rakow <meganrakow@Megans-MBP-2.lan>
2022-10-06 19:08:46 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//create variables for elements
+
+//create a function to get elements from search movie
+    //add event listener to search
+    //display movie title along with image
+
+//create a function to get playlist from searched movie
+    //display playlist picture
+    //display playlist title
+    //play song on site (hard)
+
+function movieTitle(){
+    
+}
+
+function movieAlbum(){
+
+}
