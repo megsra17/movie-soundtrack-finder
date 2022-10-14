b722cd3b1d52a66eaec2e1eea3940eac2cc3653b
Megan Rakow <meganrakow@Megans-MBP-2.lan>
2022-10-13 21:08:08 -0500

    edits to html

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var searchEl = document.querySelector('#search-form')
 var titleEl = document.querySelector('form')
 var posterEl = document.querySelector('#movie-poster')
 var posterName = document.querySelector('#posterName')
+var mainEl = document.querySelector('main');
 
 //spotify api
 const options = {
@@ -13,22 +14,15 @@ const options = {
 	}
 };
 
-//create a function to get elements from search movie
-    //add event listener to search
-    //display movie title along with image
 function searchHandler(event){
 	event.preventDefault();
 	var title = searchEl.value.trim();
+	localStorage.setItem('movie-name', title);
 
 	movieTitle(title);
 	movieAlbum(title);
 
 }
-
-//create a function to get playlist from searched movie
-    //display playlist picture
-    //display playlist title
-    //play song on site (hard)
 
 function movieTitle(title){
 	var posterApi = "https://www.myapifilms.com/imdb/idIMDB?title="+title+"&token=1eb8002a-8b27-40de-8d5b-1327dd9830dd&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=2&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=0&biography=0&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=0&actorTrivia=0&similarMovies=0&adultSearch=0&goofs=0&keyword=0&quotes=0&fullSize=1&companyCredits=0&filmingLocations=0"
@@ -38,28 +32,28 @@ function movieTitle(title){
 		return response.json();
 	})
 	.then(function(titles){
-		console.log(titles);
 		
 		posterEl.src = titles.data.movies[0].urlPoster;
 
 		posterName.textContent = titles.data.movies[0].title;
 		console.log(posterName, posterEl)
-		
-		posterEl.parentNode.insertBefore(posterName, posterEl.nextSibling);
 
-		posterName.classList.add('bottom');
-		posterName.classList.add('left');
-		posterName.classList.add('absolute');
-		posterName.classList.add('text-white');
-		posterName.classList.add('font-bold');
-		posterName.classList.add('text-8xl');
+		posterName.appendChild(mainEl)
 	})
 
 	searchEl.value = '';
 
 }
+
+//create a function to get playlist from searched movie
+    //display playlist picture
+    //display playlist title
+    //play song on site (hard)
+	
 	function movieAlbum(title){
 		var playlistapi = "https://spotify23.p.rapidapi.com/search/?q="+title+"&type=playlists&offset=0&limit=6&numberOfTopResults=6"
+
+		console.log(playlistapi);
 		
 		const options = {
 			method: 'GET',
